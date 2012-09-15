12f3c704741bf5b06c193495bf2389df2596f7e1
sebastien bodrero <sebdebillere@hotmail.fr>
2012-09-15 16:49:13 +0200

    add var folder

---
+++ jelix/beseenconcept/var/config/index/config.ini.php
@@ -0,0 +1,21 @@
+;<?php die(''); ?>
+;for security reasons , don't remove or modify the first line
+
+startModule=structure
+startAction="default:index"
+
+[responses]
+[modules]
+beseenconcept.access=2
+
+structure.access=2
+
+news.access=2
+
+jauth.access=2
+auth.access=2
+sitemap.access=2
+[coordplugins]
+auth="index/auth.coord.ini.php"
+
+
---
+++ jelix/beseenconcept/var/config/installer.ini.php
@@ -0,0 +1,48 @@
+;<?php die(''); ?>
+; for security reasons , don't remove or modify the first line
+; don't modify this file if you don't know what you do. it is generated automatically by jInstaller
+
+
+[index]
+jelix.installed=1
+jelix.version=1.4b1
+jacl.installed=0
+jacl.version=
+jacl2db.installed=0
+jacl2db.version=
+jacldb.installed=0
+jacldb.version=
+jauth.installed=1
+jauth.version=1.2.1
+jauthdb.installed=0
+jauthdb.version=
+junittests.installed=0
+junittests.version=
+jWSDL.installed=0
+jWSDL.version=
+jelix.version.date="2012-06-09 09:31"
+jelix.firstversion=1.4b1
+jelix.firstversion.date="2012-06-09 09:31"
+beseenconcept.installed=1
+beseenconcept.version=0.1pre
+structure.installed=1
+structure.version=0.1pre
+news.installed=1
+news.version=0.1pre
+jauth.version.date=2011-10-18
+jauth.firstversion=1.2.1
+jauth.firstversion.date=2011-10-18
+auth.installed=1
+auth.version=0.1pre
+sitemap.installed=1
+sitemap.version=0.1pre
+[__modules_data]
+jelix.contexts="db:default"
+
+
+
+
+jauth.contexts="auth:index/auth.coord.ini.php"
+
+
+
